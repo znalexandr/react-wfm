fec33bd7607c88815310bcbe81933d84eb2475b0
znalexandr <znalexandr@yandex.ru>
2018-12-23 19:11:49 +0300

    Создание State

--- src/App.js
+++ src/App.js
@@ -3,21 +3,26 @@ import './App.css';
 import Car from './Car/Car';
 
 class App extends Component {
+
+  state = {
+    cars: [
+      {name: 'Toyota Sprinter', year: '1988'},
+      {name: 'Hyundai Elantra', year: '2007'},
+    ]
+  }
+
   render() {
 
     const styleWrap = {
       textAlign: 'center'
     }
+    const cars = this.state.cars;
 
     return (
       <div className="App" style={styleWrap}>
         <h1>Hello world!</h1>
-        <Car name="Toyota Sprinter" year="1988">
-          <div>
-            <strong>Цвет:</strong> Белый     
-          </div>   
-        </Car>
-        <Car name="Hyundai Elantra" year="2007"/>
+        <Car name={cars[0].name} year={cars[0].year} />
+        <Car name={cars[1].name} year={cars[1].year} />
       </div>
     );
   }
